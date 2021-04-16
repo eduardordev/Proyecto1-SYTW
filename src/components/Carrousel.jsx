408785d084368965c113f85da9c4350a2d00c317
Alejandro Puente <eduardorh1312@gmail.com>
2021-04-16 17:08:23 -0600

    lets go

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Button } from 'react';
 import "./carrousel.css";
 
 
@@ -6,14 +6,38 @@ import "./carrousel.css";
 class Carrousel extends Component{
     render(){
         return(
-            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-bs-interval="7000">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  
+                </div>
+
                 <div class="carousel-inner">
+
                     <div class="carousel-item active">
                         <img src="src\imgs\LumiImage.png" class="d-block w-100" alt="..."></img>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h1>Introducing LUMI</h1>
+                            <h1>Keys Studio</h1>
+                            <h1>Edition</h1>
+                            <p>Lights, action, music</p>
+                            <button type="button" class="btn btn-warning">BUY NOW</button>
+                        </div>
                     </div>
+
                     <div class="carousel-item">
                         <img src="src\imgs\LumiImage1.png" class="d-block w-100" alt="..."></img>
-                    </div>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h4>Use code: SPRINGSALE30</h4>
+                            <h1>SPRING SALE</h1>
+                            <h2>30% OFF Synths & Sounds</h2>
+                            <button type="button" class="btn btn-primary">SHOP SALE</button>
+                            <div className="bottom-bar"></div>
+                            
+                        </div>
+                    </div>    
                 </div>
             </div>
         );
