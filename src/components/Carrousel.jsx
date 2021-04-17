9734678f96f404bcde90629b88c2947b7f5dc771
Alejandro Puente <eduardorh1312@gmail.com>
2021-04-17 01:56:58 -0600

    more components

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Button } from 'react';
+import React, { Component } from 'react';
 import "./carrousel.css";
 
 
@@ -6,34 +6,34 @@ import "./carrousel.css";
 class Carrousel extends Component{
     render(){
         return(
-            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
 
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                   
                 </div>
 
-                <div class="carousel-inner">
+                <div className="carousel-inner">
 
-                    <div class="carousel-item active">
-                        <img src="src\imgs\LumiImage.png" class="d-block w-100" alt="..."></img>
-                        <div class="carousel-caption d-none d-md-block">
+                    <div className="carousel-item active">
+                        <img src="src\imgs\LumiImage.png" className="d-block w-100" alt="..."></img>
+                        <div className="carousel-caption d-none d-md-block">
                             <h1>Introducing LUMI</h1>
                             <h1>Keys Studio</h1>
                             <h1>Edition</h1>
                             <p>Lights, action, music</p>
-                            <button type="button" class="btn btn-warning">BUY NOW</button>
+                            <button type="button" className="btn btn-warning">BUY NOW</button>
                         </div>
                     </div>
 
-                    <div class="carousel-item">
-                        <img src="src\imgs\LumiImage1.png" class="d-block w-100" alt="..."></img>
-                        <div class="carousel-caption d-none d-md-block">
+                    <div className="carousel-item">
+                        <img src="src\imgs\LumiImage1.png" className="d-block w-100" alt="..."></img>
+                        <div className="carousel-caption d-none d-md-block">
                             <h4>Use code: SPRINGSALE30</h4>
                             <h1>SPRING SALE</h1>
                             <h2>30% OFF Synths & Sounds</h2>
-                            <button type="button" class="btn btn-primary">SHOP SALE</button>
+                            <button type="button" className="btn btn-primary">SHOP SALE</button>
                             <div className="bottom-bar"></div>
                             
                         </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { LogoFacebook, LogoTwitter, LogoInstagram, LogoYoutube } from 'react-ionicons';
 import "./main.css";
+
 
 class Main extends Component{
     render(){
@@ -32,6 +34,137 @@ class Main extends Component{
                         <img src="src\imgs\seaboard.PNG" alt="" className="f14"  width="90%" height=""/>
                         <span className="expl">Explorar →</span>
                         
+                    </div>
+                </div>
+
+                <div className="art-container">
+                    <div className="title-pg">Las últimas historias de ROLI</div>
+
+                    <div className="list-t">
+                        <ul>
+                            <li>Ver todos</li>
+                            <li>Artists</li>
+                            <li>Education</li>
+                            <li>Events</li>
+                            <li>Features</li>
+                            <li>News</li>
+                            <li>Videos</li>
+                            <li>Soundpacks</li>
+                        </ul>       
+                    </div>
+
+                    <div className="articles">
+
+                        <div className="section1">
+                            <div className="bigbox">
+                                <img src="src\imgs\rafa.jpg" alt="" className="rafa" width ="100%"/>
+                                
+                                <div className="textybox">
+                                    <h2>Rafael Szaban: The sound designer from Atlantis</h2>
+                                </div>
+
+                                <div className="datext">
+                                    <p>April 16th 2021</p>
+                                    <a>Features</a>
+                                </div>
+
+                                <div className="historia">Historia principal</div>
+                                
+
+                            </div>
+                            <div className="twoverbox">
+                                <div className="boxy">
+                                    <img src="src\imgs\bluemorado.PNG" alt="" className="rafa" height ="100%" />
+
+                                    <div className="boxyb">
+                                        <div className="textybox" >
+                                            <h3>MPE: Discover the latest apps and updates</h3>
+
+                                            <div className="datext" id="datext">
+                                                <p>March 21st 2021</p>
+                                                <a>Videos</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="boxy1">
+                                    <div className="boxyb">
+                                        <div className="textybox" >
+                                            <h3>Hear the latest songs from our all-ROLI playlist</h3>
+
+                                            <div className="datext" id="datext">
+                                                <p>December 18th 2018</p>
+                                                <a>Videos</a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <img src="src\imgs\im.jpg" alt="" className="rafa" height ="100%" />  
+                                    
+                                </div>
+                                
+
+                            </div>
+
+                        </div>
+                        <div className="section2">
+                            <div className="barbox">
+
+                                <LogoFacebook
+                                    color={'#fafafa'} 
+                                    height="38px"
+                                    width="38px"
+                                />
+                                <LogoTwitter
+                                    color={'#fafafa'} 
+                                    height="38px"
+                                    width="38px"
+                                />
+                                <LogoInstagram
+                                    color={'#fafafa'} 
+                                    height="38px"
+                                    width="38px"
+                                />
+                                <LogoYoutube
+                                    color={'#fafafa'} 
+                                    height="38px"
+                                    width="38px"
+                                    
+                                />
+
+                            </div>
+                            <div className="barbox1">
+                                <input type="search" name="search" id="search" placeholder="¿Quieres enterarte de las ultimas novedades de ROLI? Regístrate"/>
+                                <input type="submit" className="submit" value="Regístrate"/>
+
+                            </div>
+
+                        </div>
+                        
+                        <div className="box">
+
+                        </div>
+                        <div className="box">
+
+                        </div>
+                        <div className="box">
+
+                        </div>
+                        <div className="box">
+
+                        </div>
+                        <div className="box">
+
+                        </div>
+                        <div className="box">
+
+                        </div>
+                        <div className="box">
+
+                        </div>
+                        <div className="box">
+
+                        </div>
                     </div>
                 </div>
             </div>
