c59b558d3365a383de016d4bb7208508d8aac964
Alejandro Puente <eduardorh1312@gmail.com>
2021-04-16 20:38:21 -0600

    some changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,14 +9,28 @@ class Main extends Component{
                 <div className="frames1">
                     <div className="f1">
 
+                        <h5>LUMI Keys Studio Edition</h5>
+                        <img src="src\imgs\lumi1.PNG" alt="" className="f11" width="90%" height=""/>
+                        <span className="expl">Explorar →</span>
+
                     </div>
                     <div className="f1">
+                        <h5 className="sft">Software</h5>
+                        <img src="src\imgs\mac.PNG" alt="" className="f12" width ="80%"/>
+                        <span className="expl">Explorar →</span>
+                    </div>
+                    <div className="f1">
+
+                        <h5>Sounds</h5>
+                        <img src="src\imgs\framis.PNG" alt="" className="f13"  width="100%" height=""/>
+                        <span className="expl">Explorar →</span>
                         
                     </div>
                     <div className="f1">
-                        
-                    </div>
-                    <div className="f1">
+
+                        <h5>Seaboard</h5>
+                        <img src="src\imgs\seaboard.PNG" alt="" className="f14"  width="90%" height=""/>
+                        <span className="expl">Explorar →</span>
                         
                     </div>
                 </div>
